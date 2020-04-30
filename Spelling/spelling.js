7f3f4f450263d8f6e14edb90f7b82594c0114366
Casey Smith <casey@caseysmith.net>
2020-04-30 04:52:52 -0600

    updating pronunciations again

--- Spelling/spelling.js
+++ Spelling/spelling.js
@@ -44,8 +44,8 @@ var initializedSpeech = 0;
 var spellingLists = [
   ["better", "follow", "happen", "different", "people", "trouble", "terrible", "awesome", "special"],
   ["enough", "through", "goes", "does", "question", "slowly", "suddenly", "probably", "trouble"],
-	["younger", "too ! . (as in also)", "to ! . (a direction)", "two ! . (a number)", "tries", "children", "different", "other", "introduction", "should", "number"],
-	["there ! . (as in ! . there are  ! . or  ! . over there)", "they're ! . (as in ! .  they ! . are.)", "their ! . (belonging to them ! .)", "turned", "intelligent", "secret", "music", "magical", "laugh", "oops"]
+	["younger", "too . ! (as in also)", "to . ! (a direction)", "two . ! (a number)", "tries", "children", "different", "other", "introduction", "should", "number"],
+	["there . ! (as in . ! there are  . ! or  . ! over there)", "they're . ! (as in . !  they . ! are.)", "their . ! (belonging to them . !)", "turned", "intelligent", "secret", "music", "magical", "laugh", "oops"]
 ];
 var pronunciationLists = [
   ["bet er", "fole ow", "happ enn", "diff fer ent", "pee-o pleh", "trow-ooh bleh", "tehr ih bleh", "awe some", "speh see-al"],
