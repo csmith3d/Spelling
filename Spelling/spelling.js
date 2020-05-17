9454dff3fdeb339aef2a4a674b712a1eebc41b1c
Casey Smith <casey@caseysmith.net>
2020-05-16 23:02:55 -0600

    added list 6

--- Spelling/spelling.js
+++ Spelling/spelling.js
@@ -46,14 +46,16 @@ var spellingLists = [
   ["enough", "through", "goes", "does", "question", "slowly", "suddenly", "probably", "trouble"],
 	["younger", "too . ! (as in also)", "to . ! (a direction)", "two . ! (a number)", "tries", "children", "different", "other", "introduction", "should", "number"],
 	["there . ! (as in) . . ! ... (there are.) ! ... (or.) ! ...  (over there)", "they're . ! ... (as in). ! ...  (they). ! ... (are. !)", "their . ! ... (as in). ! ... (belonging to them). ! ...)", "turned", "intelligent", "secret", "music", "magical", "laugh", "oops"],
-	["please", "nurse", "gem", "fairy", "fairies", "prominent", "american", "objects", "families", "trampoline"]
+	["please", "nurse", "gem", "fairy", "fairies", "prominent", "american", "objects", "families", "trampoline"],
+	["space", "rangers", "because", "choose", "chose", "guide", "really", "student", "video", "usually"]
 ];
 var pronunciationLists = [
   ["bet er", "fole ow", "happ enn", "diff fer ent", "pee-o pleh", "trow-ooh bleh", "tehr ih bleh", "awe some", "speh see-al"],
   ["ee nuff", "through", "goes", "does", "quest ion", "slow lee", "sud enn lee", "pro bab lee", "trow-ooh bleh"],
 	["yoh-un ger", "too", "to", "two", "tries", "child ren", "diff fer ent", "oth er", "in tro duct ion", "show uld", "num ber"],
 	["there", "they're", "their", "turn ed", "in tell i gent", "sec ret", "music", "mag i cal", "lah ooh guh huh", "oops"],
-	["plea ah seh", "noorse", "gem", "fair e", "fair i-ease", "pro min ent", "am er i can", "ob jects", "fam i lie-ease", "tramp o line"]
+	["plea ah seh", "noorse", "gem", "fair e", "fair i-ease", "pro min ent", "am er i can", "ob jects", "fam i lie-ease", "tramp o line"],
+	["ess pace", "rangers", "be cause", "choose", "chose", "goo ide", "real lee", "stew dent", "vid e oh", "you sue al lee"]
 ];
 
 var correct = [];
